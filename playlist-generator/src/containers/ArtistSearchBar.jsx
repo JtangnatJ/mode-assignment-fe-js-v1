11aaf7f1430278a5c3c0a1a9c7f9dc85704f3460
Jacky Tang <jackytang74@gmail.com>
2022-01-20 21:17:53 -0800

    added file for layout

---
+++ playlist-generator/src/containers/ArtistSearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ArtistSearchBar = () => {
+    return (
+        <div className="artistSearchBar">
+            ArtistSearchBar
+            {/* TODO: test query */}
+        </div>
+    );
+};
